c0d8d1bde68c90fa666b150a918781be244cda93
Danio Fiore <danio.fiore@sysco.biz>
2024-09-13 10:44:32 +0200

    created account.py where we generate the public key and private key and create a bitcoin address

--- Blockchain/server/tools/tools.py
+++ Blockchain/server/tools/tools.py
@@ -1,7 +1,11 @@
 import hashlib
+from Crypto.Hash import RIPEMD160
+from hashlib import sha256
 
 # hash256 its 2 rounds of sha256. It will create the hash and the hash that hash to create a new hash
 def hash256(string):
     return hashlib.sha256(hashlib.sha256(string).digest()).digest()
 
+def hash160(string):
+    return RIPEMD160.new(sha256(string).digest()).digest()
 # to allow us to import this file as a package, we have to create in utils folder, a file called __init__.py
